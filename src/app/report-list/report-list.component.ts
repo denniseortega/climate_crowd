6d5bcb91adf6ef4dde15fda206924205f5845b60
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-08 14:34:27 -0700

    git rid of app edit report since component doesn't exists yet

--- src/app/report-list/report-list.component.ts
+++ src/app/report-list/report-list.component.ts
@@ -10,33 +10,10 @@ export class ReportListComponent {
   @Input() childReportList: Report[];
   @Output() clickSender = new EventEmitter();
 
-  editButtonClicked(reportToEdit: Report) {
-    this.clickSender.emit(reportToEdit);
-  }
-
   reports: Report[] = [
     new Report('Jun 8, 2018','SAVE Organization','Alex Smith', 'alex@email.com'),
     new Report('May 24, 2018','HELP Organization', 'Mary Joe','mary@email.com'),
     new Report('April 13, 2018', 'RAISE Organization', 'Tom Jeff', 'tom@gmail.com')
   ];
 
-  filterByCompleteness: string = "incompleteReports";
-
-  onChange(optionFromMenu) {
-  this.filterByCompleteness = optionFromMenu;
-  }
-
-  priorityColor(currentReport){
-    if (currentReport.priority === 3){
-      return "bg-danger";
-    } else if (currentReport.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-
-  toggleDone(clickedReport: Report, setCompleteness: boolean) {
-     clickedReport.done = setCompleteness;
-   }
 }
