400afc80ac2d5ef67c0b8297ef74d4ca33e1425e
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-08 16:14:18 -0700

    remove parameter prperties from class NewTaskComponent

--- src/app/new-report/new-report.component.ts
+++ src/app/new-report/new-report.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Report } from '../models/report.model';
 
 @Component({
   selector: 'app-new-report',
   templateUrl: './new-report.component.html',
   styleUrls: ['./new-report.component.css']
 })
-export class NewReportComponent implements OnInit {
+export class NewReportComponent {
+  @Output() sendReport = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  submitForm (date: string, organization: string, name: string, email: string) {
+    let newReport: Report = new Report(date, organization, name, email);
+    this.sendReport.emit(newReport);
   }
-
 }
