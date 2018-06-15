b3340474df94ac2fda5614dfbb79aad3683dc055
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-15 12:40:05 -0700

    welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ReportListComponent } from './report-list/report-list.component';
 import { NewReportComponent } from './new-report/new-report.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ReportListComponent,
-    NewReportComponent
+    NewReportComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
