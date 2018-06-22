06ce1660f5691d85e831baadf95d9d075fd62cb6
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-21 21:05:19 -0700

    add routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,23 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { NewReportComponent } from './new-report/new-report.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {
       path: '',
       component: WelcomeComponent
-    }
+    },
+  {
+    path: 'newreport',
+    component: NewReportComponent
+   },
+ {
+   path: 'about',
+   component: AboutComponent
+  }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
